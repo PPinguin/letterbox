b239292f6e69c6e470ce8cddde380d9a8533eca8
PPinguin <petrof.v.a.0910@gmail.com>
2025-07-31 10:58:05 +0200

    pandora, sun

--- main.js
+++ main.js
@@ -1,27 +1,29 @@
 const delay = ms => new Promise(res => setTimeout(res, ms));
-let isOpened = false;
+let isOpenedMap = {};
 
 async function openEnvelope(index) {
+    if(isOpenedMap[index] == null) {
+        isOpenedMap[index] = false;
+    }
     const envelope = document.getElementById(`envelope${index}`);
 
     const flap = document.querySelector(`#envelope${index} .envelope-flap`);
     const letter = document.querySelector(`#envelope${index} .letter`);
 
-    if (!isOpened) {
+    if (!isOpenedMap[index]) {
         envelope.classList.add('opened');
         await delay(500);
         flap.style.zIndex = 1;
         letter.classList.add('opened');
 
-        isOpened = true;
+        isOpenedMap[index] = true;
 
-        // Add some sparkle effect
     } else {
         letter.classList.remove('opened');
         await delay(500);
         flap.style.zIndex = 4;
         envelope.classList.remove('opened');
 
-        isOpened = false;
+        isOpenedMap[index] = false;
     }
 }
